f4df739dfe596656d0d7976e4a4f963a5785f982
Gleb HP <podavalovg@gmail.com>
2021-07-25 12:26:22 +0300

    lesson 2 start

---
+++ Lesson02/lesson 2/lesson 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите минимальную темпиратуру:");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите максимальную темпиратуру:");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int mid = (min + max) / 2;
+            Console.WriteLine($"Средняя темпиратура воздуха за день {mid} градусов");
+        }
+    }
+}
