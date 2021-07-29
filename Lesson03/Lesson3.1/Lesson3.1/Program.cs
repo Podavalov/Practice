257e9c8bb74731d10d2f4fa976382df145572e72
Gleb HP <podavalovg@gmail.com>
2021-07-29 09:33:27 +0300

    завершил первое задание

--- Lesson03/Lesson3.1/Lesson3.1/Program.cs
+++ Lesson03/Lesson3.1/Lesson3.1/Program.cs
@@ -6,7 +6,17 @@ namespace Lesson3._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] matrix = new int[5, 5];
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.WriteLine($"{new string(' ', i)}{matrix[i, j]}");
+                    break;
+                }
+            }
         }
     }
 }
