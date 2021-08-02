a0d7a471f98dd55f59ad4a161f56ec53e16156ea
Gleb HP <podavalovg@gmail.com>
2021-08-02 10:48:14 +0300

    Закончил делать первое задание

--- Lesson04/Lesson4.1/Lesson4.1/Program.cs
+++ Lesson04/Lesson4.1/Lesson4.1/Program.cs
@@ -6,7 +6,17 @@ namespace Lesson4._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(GetFullName("Иванович", "Фёдр", "Степанович"));
+            Console.WriteLine(GetFullName("Николаев", "Станислав", "Дмитриевич"));
+            Console.WriteLine(GetFullName("Петров", "Егор", "Сергеевич"));
+            Console.WriteLine(GetFullName("Алексеев", "Алексей", "Александрович"));
         }
+
+        static string GetFullName(string lastName, string firstName, string patronymic)
+        {
+            return $"{lastName} {firstName} {patronymic}";
+        }
+
+
     }
 }
